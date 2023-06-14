ed17ee1ec157fee4e772c42894d1badf9f9d1a70
pepemalo <andercardozo04@gmail.com>
2023-06-13 19:17:35 -0500

    Visualizar datos cargados por metodo manual probando con js..

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,7 +10,8 @@ import io
 import os
 
 
-
+#estilo para ver la carga de archivo
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
@@ -387,10 +388,10 @@ def render_page_content(pathname):
                         "left": '20px',
                     },
                     # Allow multiple files to be uploaded
-                    multiple=False,
+                    multiple=True,
                 ),
                 html.Div(id='output-data-upload'),
-                html.Div(id='popup', style={'display': 'none'})
+                #html.Div(id='popup', style={'display': 'none'})
             ]),
             html.Div([
                 dcc.DatePickerRange(
@@ -509,50 +510,53 @@ def render_page_content(pathname):
     )
 
 #Despliegue de pagina con los datos a graficar
-app.scripts.append_script({
-    'external_url': 'https://code.jquery.com/jquery-3.6.0.min.js'
-})
-app.clientside_callback(
-    """
-    function(displayPopup, outputData) {
-        if (outputData) {
-            displayPopup();
-        }
-    }
-    """,
-    Output('popup', 'style'),
-    [Input('output-data-upload', 'children')],
-    prevent_initial_call=True
-)
-
-
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              )
-def update_output(contents):
-    if contents is not None:
-        children = parse_contents(contents)
-        return children
-
-
-def parse_contents(contents):
+def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
 
     decoded = base64.b64decode(content_string)
-    
     try:
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
     except Exception as e:
         print(e)
         return html.Div([
-            'Hubo un error al procesar el archivo.'
+            'There was an error processing this file.'
         ])
 
     return html.Div([
-        html.H5("Contenido del archivo:"),
-        html.Pre(df.head().to_string())
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            df.to_dict('records'),
+            [{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
     ])
 
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 if __name__ == ('__main__'):
     app.run_server(debug=False)
